511465a130e4da5394aea6d869ac2df2241600ee
Moong0122 <2015004802@hanyang.ac.kr>
2020-07-20 15:49:25 +0900

    CSS styles -> difficult part (Studying repeatedly)

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
-import Header from "Components/Header";
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends Component {
   render() {
     return (
       // 밑에 있는 것은 fragment라고 부른다 내가 원하는 만큼 component를 return 할 수 있도록 해준다.
       <>
-        <Header />
         <Router />
+        <GlobalStyles />
       </>
     );
   }
